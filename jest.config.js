1ae8f952feae3242721310063ad141d892c8a3fe
Brayan Camargo <brayanochz@gmail.com>
2023-10-28 08:59:38 -0500

    🎉 Setup CryptoUSD project

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const nextJest = require("next/jest")
+const createJestConfig = nextJest({
+  dir: "./src/"
+})
+
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environments-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig)
