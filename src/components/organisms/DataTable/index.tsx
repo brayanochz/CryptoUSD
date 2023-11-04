9d9e2dda126740429c3abd248f763038f7f4688e
Brayan Camargo <brayanochz@gmail.com>
2023-11-04 12:19:03 -0500

    ♻️ refactor: Improve data filtering logic

--- src/components/organisms/DataTable/index.tsx
+++ src/components/organisms/DataTable/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import Table from '../../molecules/Table';
 import { CryptocurrencyClient } from '@/services/CryptocurrencyClient';
 import { FilterType } from '@/types';
+import { Coin } from '@/interfaces/coins';
 
 const columns = {
   Name: {
@@ -20,24 +21,16 @@ const columns = {
 }
 
 interface DataTableProps {
-  page: string,
-  filter: FilterType
+  data: Coin[]
 }
 
-const DataTable: FC<DataTableProps> = async ({ page, filter }) => {
+const DataTable: FC<DataTableProps> = async ({ data }) => {
 
-  const cryptocurrencyClient = new CryptocurrencyClient()
 
-  const coins = await cryptocurrencyClient.getCoins(page)
-
-  const filteredData = coins.data.filter((coin) => {
-    const nameFilter = coin.name.toLowerCase().includes((filter.name as string)?.toLowerCase())
-    return nameFilter
-  })
 
   return (
     <section className="relative max-h-70vh overflow-x-auto w-full">
-      <Table data={filteredData} headers={columns} />
+      <Table data={data} headers={columns} />
     </section>
   );
 };
--- src/components/templates/CoinList/index.tsx
+++ src/components/templates/CoinList/index.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
-import OptionBar from '../../molecules/OptionBar';
+import OptionBar from '../../organisms/OptionBar';
 import DataTable from '../../organisms/DataTable';
 import { FilterType } from '@/types';
+import { CryptocurrencyClient } from '@/services/CryptocurrencyClient';
+import { Coin } from '@/interfaces/coins';
 
 interface CoinListProps {
   page: string,
@@ -10,10 +12,30 @@ interface CoinListProps {
 
 const CoinList: FC<CoinListProps> = async ({ page, filter }) => {
 
+  const cryptocurrencyClient = new CryptocurrencyClient()
+
+  const coins = await cryptocurrencyClient.getCoins(page)
+
+  const arrayFilter = Object.keys(filter)
+
+  const filteredData = arrayFilter.reduce<Coin[]>(
+    (previous, current) => {
+      if (previous?.length <= 0) {
+        return coins.data.filter((coin: Coin) => {
+          return coin[current as keyof Coin].toString().toLowerCase().includes((filter[current] as string)?.toString().toLowerCase())
+        })
+      }
+      return previous.filter((coin: Coin) => {
+        return coin[current as keyof Coin].toString().toLowerCase().includes((filter[current] as string)?.toString().toLowerCase())
+      })
+    },
+    []
+  )
+
   return (
     <div>
       <OptionBar filter={filter} />
-      <DataTable page={page} filter={filter} />
+      <DataTable data={filteredData} />
     </div>
   );
 };
