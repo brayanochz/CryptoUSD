7061c85b51e7327e3406a59ac83917aa5de835b5
Brayan Camargo <brayanochz@gmail.com>
2023-11-07 20:21:43 -0500

    🧪 test: update test for currency hook

--- src/_tests_/hooks/useCryptoCurrency.test.tsx
+++ src/_tests_/hooks/useCryptoCurrency.test.tsx
@@ -1,5 +1,5 @@
 import useCryptoCurrency from "@/hooks/useCryptoCurrency";
-import { cryptoCurrency } from "@/mock/crypto";
+import { cryptoCurrency, cryptoDetail } from "@/mock/crypto";
 import { CryptocurrencyClient } from "@/services/CryptocurrencyClient";
 
 // Mock the CryptocurrencyClient module
@@ -7,6 +7,7 @@ jest.mock("@/services/CryptocurrencyClient");
 
 describe('useCryptoCurrency', () => {
   CryptocurrencyClient.prototype.getCoins = jest.fn().mockResolvedValue(cryptoCurrency);
+  CryptocurrencyClient.prototype.getCoinDetails = jest.fn().mockResolvedValue(cryptoDetail);
 
   it('should return coins data when getCoins is called', async () => {
     const { getCoins } = useCryptoCurrency();
@@ -16,6 +17,16 @@ describe('useCryptoCurrency', () => {
 
     expect(CryptocurrencyClient.prototype.getCoins).toHaveBeenCalledWith(page);
     expect(data).toEqual(cryptoCurrency);
+  });
+
+  it('should return coins data when getCoins is called', async () => {
+    const { getCoinDetails } = useCryptoCurrency();
+    const id = '50';
+
+    const data = await getCoinDetails(id);
+
+    expect(CryptocurrencyClient.prototype.getCoinDetails).toHaveBeenCalledWith(id);
+    expect(data).toEqual(cryptoDetail);
   });
 
 });
