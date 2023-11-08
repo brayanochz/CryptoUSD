4b979ea8c9ffa623dfac90978164130b02e21f67
Brayan Camargo <brayanochz@gmail.com>
2023-11-07 22:21:38 -0500

    🧪 test: update test for markets

--- src/_tests_/app/details/page.test.tsx
+++ src/_tests_/app/details/page.test.tsx
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen, waitFor } from '@testing-library/react';
 import Details from '@/app/details/[id]/page';
 import useCryptoCurrency from "@/hooks/useCryptoCurrency";
-import { cryptoDetailMock } from "@/mock/crypto";
+import { cryptoDetailMock, marketsMock } from "@/mock/crypto";
 import { CryptocurrencyClient } from "@/services/CryptocurrencyClient";
 
 // Hacer un mock de CryptocurrencyClient
@@ -19,6 +19,7 @@ jest.mock("@/hooks/useCryptoCurrency", () => {
     return {
       ...originalModule,
       getCoinDetails: jest.fn().mockResolvedValue(cryptoDetailMock),
+      getMarkets: jest.fn().mockResolvedValue(marketsMock),
     };
   });
 });
@@ -29,70 +30,65 @@ const mockParams = {
 
 describe('Details Component', () => {
   it('renders without crashing', async () => {
-
     render(await Details(mockParams))
-
-    await waitFor(() => {
-      expect(screen.getByText(/Details of Bitcoin \(BTC\)/i)).toBeVisible();
-    });
-  });
-
-  it('displays the correct id', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
-    render(await Details(mockParams))
-    expect(screen.getByText(mockParams.params.id)).toBeVisible();
+    expect(screen.getByText(/Details of Bitcoin \(BTC\)/i)).toBeVisible();
   });
 
   it('renders the price information correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/\$35\,351\.45/i)).toBeVisible();
   });
 
   it('renders the 24h change correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/0\.75%/i)).toBeVisible();
   });
 
   it('renders the 1h change correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/-0\.48%/i)).toBeVisible();
   });
 
   it('renders the 7d change correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/2\.70%/i)).toBeVisible();
   });
 
   it('renders the market cap correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/\$688,326,163,100/i)).toBeVisible();
   });
 
   it('renders the volume (24h) correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/\$16,112,887,740/i)).toBeVisible();
   });
 
   it('renders the market section correctly', async () => {
-    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetailMock);
-
     render(await Details(mockParams))
     expect(screen.getByText(/Markets/i)).toBeVisible();
-    expect(screen.getByText(/BitForex/i)).toBeVisible();
-    expect(screen.getByText(/bw/i)).toBeVisible();
+  });
+
+  it('renders the market name correctly', async () => {
+    render(await Details(mockParams))
+    expect(screen.getByText(/BingX/i)).toBeVisible();
+  });
+
+  it('renders the market base/quote correctly', async () => {
+    render(await Details(mockParams))
+    expect(screen.getByText(/BTC\/USDT/i)).toBeVisible();
+  });
+
+
+  it('renders the market value correctly', async () => {
+    render(await Details(mockParams))
+    expect(screen.getByText(/\$35,330\/ USD/i)).toBeVisible();
+  });
+
+
+  it('renders the market volume correctly', async () => {
+    render(await Details(mockParams))
+    expect(screen.getByText(/Volume:\$2.266904803B/i)).toBeVisible();
   });
 
 });
