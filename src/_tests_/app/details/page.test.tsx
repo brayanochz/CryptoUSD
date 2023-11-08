636f7de83e654455baff838bef04a15673e280a1
Brayan Camargo <brayanochz@gmail.com>
2023-11-07 21:16:55 -0500

    🧪 test: add failing tests for detail page

---
+++ src/_tests_/app/details/page.test.tsx
@@ -0,0 +1,98 @@
+import "@testing-library/jest-dom";
+import { render, screen, waitFor } from '@testing-library/react';
+import Details from '@/app/details/[id]/page';
+import useCryptoCurrency from "@/hooks/useCryptoCurrency";
+import { cryptoDetail } from "@/mock/crypto";
+import { CryptocurrencyClient } from "@/services/CryptocurrencyClient";
+
+// Hacer un mock de CryptocurrencyClient
+jest.mock("@/services/CryptocurrencyClient", () => ({
+  CryptocurrencyClient: jest.fn().mockImplementation(() => ({
+    getCoinDetails: jest.fn().mockResolvedValue(cryptoDetail),
+  })),
+}));
+
+// Hacer un mock del hook useCryptoCurrency
+jest.mock("@/hooks/useCryptoCurrency", () => {
+  return jest.fn(() => {
+    const originalModule = jest.requireActual("@/hooks/useCryptoCurrency");
+    return {
+      ...originalModule,
+      getCoinDetails: jest.fn().mockResolvedValue(cryptoDetail),
+    };
+  });
+});
+
+const mockParams = {
+  params: { id: '90' }
+};
+
+describe('Details Component', () => {
+  it('renders without crashing', async () => {
+
+    render(await Details(mockParams))
+
+    await waitFor(() => {
+      expect(screen.getByText(/Details of Bitcoin \(BTC\)/i)).toBeVisible();
+    });
+  });
+
+  it('displays the correct id', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(mockParams.params.id)).toBeVisible();
+  });
+
+  it('renders the price information correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/\$35\,351\.45/i)).toBeVisible();
+  });
+
+  it('renders the 24h change correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/0\.75%/i)).toBeVisible();
+  });
+
+  it('renders the 1h change correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/-0\.48%/i)).toBeVisible();
+  });
+
+  it('renders the 7d change correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/2\.70%/i)).toBeVisible();
+  });
+
+  it('renders the market cap correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/\$688,326,163,064\.15/i)).toBeVisible();
+  });
+
+  it('renders the volume (24h) correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/\$16,112,887,735\.641008/i)).toBeVisible();
+  });
+
+  it('renders the market section correctly', async () => {
+    useCryptoCurrency.prototype.getDetails = jest.fn().mockResolvedValue(cryptoDetail);
+
+    render(await Details(mockParams))
+    expect(screen.getByText(/Markets/i)).toBeVisible();
+    expect(screen.getByText(/BitForex/i)).toBeVisible();
+    expect(screen.getByText(/bw/i)).toBeVisible();
+  });
+
+});
