2539ac47fbe06495ef155eb43e749b4be35b8f81
Brayan Camargo <brayanochz@gmail.com>
2023-11-08 10:56:25 -0500

    📝 docs: improve code documentation

--- src/hooks/useCryptoCurrency.tsx
+++ src/hooks/useCryptoCurrency.tsx
@@ -1,17 +1,47 @@
 import { CryptocurrencyClient } from "@/services/CryptocurrencyClient";
 
+/**
+ * This is a custom hook that provides functions to fetch cryptocurrency data from an API.
+ * @returns The `useCryptoCurrency` function returns an object with three properties: `getCoins`,
+ * `getCoinDetails`, and `getMarkets`.
+ */
 const useCryptoCurrency = () => {
 
   const cryptocurrencyClient = new CryptocurrencyClient()
 
+ /**
+  * The above code defines three async functions that make API calls to retrieve cryptocurrency data.
+  * @param {string} page - The `page` parameter is a string that represents the page number or
+  * identifier for retrieving a list of coins. It is used in the `getCoins` function to fetch a list of
+  * coins from the cryptocurrency client.
+  * @returns The functions `getCoins`, `getCoinDetails`, and `getMarkets` are returning promises that
+  * will resolve to the results of the corresponding API calls made by the `cryptocurrencyClient`
+  * object.
+  */
   const getCoins = async (page: string) => {
     return await cryptocurrencyClient.getCoins(page)
   }
 
+  /**
+   * The function `getCoinDetails` is an asynchronous function that retrieves details of a
+   * cryptocurrency coin based on its ID.
+   * @param {string} coinId - The `coinId` parameter is a string that represents the unique identifier
+   * of a cryptocurrency.
+   * @returns The function `getCoinDetails` is returning the result of the
+   * `cryptocurrencyClient.getCoinDetails` function, which is awaited using the `await` keyword.
+   */
   const getCoinDetails = async (coinId: string) => {
     return await cryptocurrencyClient.getCoinDetails(coinId)
   }
 
+  /**
+   * The function `getMarkets` is an asynchronous function that retrieves markets data for a given coin
+   * ID using the `cryptocurrencyClient` API.
+   * @param {string} coinId - The `coinId` parameter is a string that represents the unique identifier
+   * of a cryptocurrency.
+   * @returns The `getMarkets` function is returning a promise that resolves to the result of calling
+   * `cryptocurrencyClient.getMarkets(coinId)`.
+   */
   const getMarkets = async (coinId: string) => {
     return await cryptocurrencyClient.getMarkets(coinId)
   }
--- src/hooks/useCustomRouter.tsx
+++ src/hooks/useCustomRouter.tsx
@@ -6,11 +6,29 @@ export interface CustomRouterOptions {
   preserveQuery: boolean;
 }
 
-// Custom hook for extending Next.js router functionality
+/**
+ * The `useCustomRouter` hook provides access to a customized router instance in a Next.js application.
+ * It allows you to navigate between pages, access router-related functionality, and includes a `push` function
+ * for programmatic navigation with optional query parameter preservation and additional navigation options.
+ *
+ * @returns {{
+*   push: (href: string, routerOptions?: CustomRouterOptions, options?: NavigateOptions) => void
+* }} - An object containing methods and properties for custom navigation and router handling.
+*/
 export function useCustomRouter() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
+  /**
+   * The `push` function is used for programmatic navigation in a Next.js application. It allows you to navigate to a specified URL
+   * while optionally preserving the query parameters and providing additional navigation options.
+   *
+   * @param {string} href - The URL you want to navigate to.
+   * @param {CustomRouterOptions} [routerOptions] - An optional object containing custom router options.
+   * @param {NavigateOptions} [options] - An optional object containing navigation options.
+   *
+   * @returns {void} - This function does not return a value; it performs the navigation action.
+   */
   const push = (href: string, routerOptions?: CustomRouterOptions, options?: NavigateOptions) => {
 
     const actualSearchParams = searchParams.toString()
