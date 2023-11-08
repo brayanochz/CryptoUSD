50e9dc14e5f5bfd3a9cdea3ccdea4a21123cc02f
Brayan Camargo <brayanochz@gmail.com>
2023-11-07 21:17:25 -0500

    🤡 test: add mock data

--- src/mock/crypto.ts
+++ src/mock/crypto.ts
@@ -63,3 +63,24 @@ export const cryptoCurrency = {
     "time": 1538560355
   }
 }
+
+export const cryptoDetail = [
+  {
+    "id": "90",
+    "symbol": "BTC",
+    "name": "Bitcoin",
+    "nameid": "bitcoin",
+    "rank": 1,
+    "price_usd": "35351.45",
+    "percent_change_24h": "0.75",
+    "percent_change_1h": "-0.48",
+    "percent_change_7d": "2.70",
+    "price_btc": "1.00",
+    "market_cap_usd": "688326163064.15",
+    "volume24": 16112887735.641008,
+    "volume24a": 11880579707.878819,
+    "csupply": "19470946.00",
+    "tsupply": "19470946",
+    "msupply": "21000000"
+  }
+]
